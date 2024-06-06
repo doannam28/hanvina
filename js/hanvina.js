86a0936e43c130157ae0faddf640bd7f0d60cc53
doannam28 <doannam28@gmail.com>
2024-06-06 16:00:48 +0700

    Add file

---
+++ js/hanvina.js
@@ -0,0 +1,23 @@
+var HANVINA = {
+    init: function(){
+// Get the video
+        var video = document.getElementById("myVideo");
+
+// Get the button
+        var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+        function myFunction() {
+            if (video.paused) {
+                video.play();
+                btn.innerHTML = "Pause";
+            } else {
+                video.pause();
+                btn.innerHTML = "Play";
+            }
+        }
+    }
+}
+jQuery.document.ready(function(){
+    HANVINA.init()
+})
